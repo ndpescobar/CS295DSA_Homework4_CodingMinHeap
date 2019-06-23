86b65b20e22e65daa589ac8da32dc2becf0b5c35
Nicolas <ndpescobar@gmail.com>
2019-06-22 22:32:04 -0700

    Initial Setup
    Visual Studio Project setup for my Data Structures and Algorithms class.
    Note: Testing with pushing whole project folder. Possible different work setups and versions of VS.

---
+++ CS295_Homework4_coding/CS295_Homework4_coding/CS295DSA_NPlataEscobar_Homework4_MinHeap_Source.cpp
@@ -0,0 +1,15 @@
+/*	Homework 4 Minimum Heap Implementation.
+	@author: Nicolas Plata-Escobar.
+	@version: vBuild 1; 22 June 2019.
+*/
+#include <iostream>
+#include <string>
+
+int main() {
+
+	std::cout << "Double ENTER to EXIT:";
+	string EXIT;
+	getline(cin, EXIT);
+	getline(cin, EXIT);
+	return 0;
+}
